a41158f752d626d1b1ce22a03cc7cd5fe4b5936c
jianghuajie1983@163.com <jianghuajie1983@163.com>
2026-08-14 21:14:05 +0800

    release: v1.0.302

--- main.py
+++ main.py
@@ -90,8 +90,8 @@ else:
 tray_manager = None
 
 # ========== 2. 版本信息 ==========
-APP_VERSION = "1.0.301"
-APP_VERSION_CODE = 301
+APP_VERSION = "1.0.302"
+APP_VERSION_CODE = 302
 # =============================
 
 # ========== 3. 设备绑定功能 ==========
